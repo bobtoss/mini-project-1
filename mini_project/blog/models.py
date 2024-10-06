d9659f3c5383afa79b60b0ee1b6b97f182efc51c
_ <orazgalialmas@gmail.com>
2024-10-06 23:38:16 +0600

    + models

--- mini_project/blog/models.py
+++ mini_project/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from mini_project.users.models import User
+
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(100)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(Post, on_delete=models.CASCADE)
--- mini_project/users/models.py
+++ mini_project/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+def user_directory_path(instance):
+    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+    return 'user_{0}'.format(instance.user.id)
+
+
+class User(models.Model):
+    username = models.TextField(100)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    bio = models.TextField()
+    picture = models.ImageField(upload_to=user_directory_path)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE(), primary_key=True)
+    follower = models.ForeignKey(User, on_delete=models.CASCADE)
+    following = models.ForeignKey(User, on_delete=models.CASCADE)
